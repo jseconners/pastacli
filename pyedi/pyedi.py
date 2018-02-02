00a66b303efa066b9a4107068b35d3b25aa41fac
James Conners <jseconners@gmail.com>
2018-02-02 11:09:56 -0800

    Created script skeleton

---
+++ pyedi/pyedi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+######################################################
+#
+# pyedi - Python CLI for interacting with the Environmental
+#         Data Initiative's (EDI) API for archiving data
+# Written by James Conners (jseconners@gmail.com)
+#
+######################################################
+
+import sys
+import os
+import json
+import argparse
+import re
+from datetime import datetime, timedelta
+from urlparse import urljoin
+import random
+
+from bs4 import BeautifulSoup
+import requests
+
+
+
+def command_line_runner():
+    """ Function run if run as script """
+
+
+if __name__ == '__main__':
+    command_line_runner()
