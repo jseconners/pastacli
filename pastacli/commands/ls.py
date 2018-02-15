70eb3567037110949e17c72f5aadb53904dbe0f6
James Conners <jseconners@gmail.com>
2018-02-15 12:57:29 -0800

    Adding some utilities and more sub commands for listing

--- pastacli/commands/ls.py
+++ pastacli/commands/ls.py
@@ -1,13 +1,61 @@
 import os
+import sys
 import json
 import click
 import pastacli.utils.core as ucore
 
 
-@click.group()
+@click.group('list')
 def ls():
     pass
 
-@ls.command('stub')
-def stub():
-    click.echo("Sub command stub")
+@ls.command('data-entities')
+@click.argument('scope')
+@click.argument('id')
+@click.argument('revision')
+def list_data_entities(scope, id, revision):
+    url = ucore.make_url('package/data/eml', scope, id, revision)
+    res = ucore.get(url)
+    ucore.status_check(res, [200])
+    click.echo(res.status_code)
+
+
+@ls.command('data-descendants')
+@click.argument('scope')
+@click.argument('id')
+@click.argument('revision')
+def list_data_descendants(scope, id, revision):
+    url = ucore.make_url('package/descendants/eml', scope, id, revision)
+    res = ucore.get(url)
+    ucore.status_check(res, [200])
+    click.echo(res.text)
+
+
+@ls.command('data-sources')
+@click.argument('scope')
+@click.argument('id')
+@click.argument('revision')
+def list_data_sources(scope, id, revision):
+    url = ucore.make_url('package/sources/eml', scope, id, revision)
+    res = ucore.get(url)
+    ucore.status_check(res, [200])
+    click.echo(res.text)
+
+
+@ls.command('package-identifiers')
+@click.argument('scope')
+def list_package_identifiers(scope):
+    url = ucore.make_url('package/eml', scope)
+    res = ucore.get(url)
+    ucore.status_check(res, [200])
+    click.echo(res.text)
+
+
+@ls.command('package-scopes')
+@click.argument('scope')
+@click.argument('id')
+def list_package_identifiers(scope, id):
+    url = ucore.make_url('package/eml', scope, id)
+    res = ucore.get(url)
+    ucore.status_check(res, [200])
+    click.echo(res.text)
--- pastacli/utils/core.py
+++ pastacli/utils/core.py
@@ -1,6 +1,7 @@
 import os
+import click
 import requests
-from urllib.parse import urljoin
+from urllib.parse import urlencode
 
 BASE_URL = 'https://pasta.lternet.edu'
 CONFIG_DIR = os.path.join(os.path.expanduser('~'), '.pastacli')
@@ -10,17 +11,22 @@ AUTH_FILE = os.path.join(CONFIG_DIR, '.auth')
 # UTILITIES
 ################################################################################
 
-def _get(url, **opts):
-    """ Return response content and HTTP code """
+def make_url(*parts, query={}):
+    url = "/".join([p.strip('/') for p in [BASE_URL] + list(parts)])
+    if query:
+        url = "{}?{}".format(url, urlencode(query))
+    return url
+
+
+def get(url):
     try:
-        res = requests.get(url, auth=creds, **opts)
+        res = requests.get(url)
     except requests.exceptions.RequestException:
         print("Failed retrieving {}".format(url)) >> sys.stderr
         sys.exit(1)
     return res
 
-def _post(url, **opts):
-    """ Return response content and HTTP code """
+def post(url, **opts):
     try:
         res = requests.post(url,
                            auth=creds,
@@ -33,10 +39,9 @@ def _post(url, **opts):
     return res
 
 
-def status_check(res, expected):
-    if (res.status_code != expected):
+def status_check(res, expected=[]):
+    if (expected and (res.status_code not in expected)):
         try:
             raise res.raise_for_status()
         except requests.exceptions.HTTPError as e:
-            print(e)
-            sys.exit(1)
+            click.echo(e)
